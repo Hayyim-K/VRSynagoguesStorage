174a59b27a76a998823d1fc10ce13be948d16de2
Hayyim <b2banalytica@gmail.com>
2023-07-06 14:18:04 +0100

    v 2.1 - rewrote the code according to Apple recommendation - added the title to the final VC to display the name of the temple

--- testAPIApp/ViewController.swift
+++ testAPIApp/ViewController.swift
@@ -18,19 +18,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         lodeWebContent()
     }
+    
     private func lodeWebContent() {
         DispatchQueue.global().async {
-            print("1")
-            print(self.stringURL ?? "ERROR")
             guard let url = URL(string: self.stringURL) else { return }
             guard let htmlString = try? String(contentsOf: url) else { return }
             DispatchQueue.main.async {
                 self.wkWebView.loadHTMLString(htmlString, baseURL: url)
-                print("2")
-                print(url)
             }
         }
     }
-
+    
 }
 
